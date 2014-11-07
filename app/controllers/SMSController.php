c8f6420205d29e23dcc0549faa2fd5807c30c590
Habibie Faried <habibiefaried@gmail.com>
2014-11-07 13:18:51 +0700

    Mindahin kalgoritma SMS ke eloquent model laravel

--- app/controllers/SMSController.php
+++ app/controllers/SMSController.php
@@ -1,37 +1,24 @@
 <?php
+
+App::bind('SMS','SMS');
+
 class SMSController extends BaseController {
 	private $API_URL = 'https://api.twilio.com/2010-04-01/Accounts/AC17184cedf0246af21424ebe4a6a2048c/Messages.json';
 	private $API_AUTH = 'AC17184cedf0246af21424ebe4a6a2048c:3ce905ab2880d0e46879f527146d2401';
-	public function __construct(){
+	private $SMS; //ngeload Model SMS
 
+	public function __construct(SMS $SMS){
+		$this->SMS = $SMS;
 	}
 
 	public function send($body) {
-		$TO = '+628561435232';
-		$FROM = '+12027596195';
-		$output = shell_exec("curl -X POST '".$this->API_URL."' --data-urlencode 'To=".$TO."' --data-urlencode 'From=".$FROM."' --data-urlencode 'Body=".$body."' -u ".$this->API_AUTH);
-		var_dump($output);
+		$res = $this->SMS->send($body);
+		var_dump($res);
 	}
 
 	public function recv(){
 		/* Get received messages */
-		$output = shell_exec("curl -X GET '".$this->API_URL."' -u ".$this->API_AUTH);
-		$hasil_json = json_decode($output);
-
-		foreach ($hasil_json->messages as $row) {
-			if ($row->status == "received") {
-				echo "Pesan : ".$row->body."<br>";
-			}
-		}
-	}
-
-	public function recvdebug() {
-		//->sid, ->date_created, ->date_updated, ->date_sent
-		$output = shell_exec("curl -X GET '".$this->API_URL."' -u ".$this->API_AUTH);
-		$hasil_json = json_decode($output);
-
-		foreach ($hasil_json->messages as $row) {
-			var_dump($row);
-		}
+		$res = $this->SMS->recv();
+		var_dump($res);
 	}
 }
---
+++ app/models/SMS.php
@@ -0,0 +1,34 @@
+<?php
+class SMS extends Eloquent {
+	private $API_URL = 'https://api.twilio.com/2010-04-01/Accounts/AC17184cedf0246af21424ebe4a6a2048c/Messages.json';
+	private $API_AUTH = 'AC17184cedf0246af21424ebe4a6a2048c:3ce905ab2880d0e46879f527146d2401';
+
+	public function send($body) {
+		$TO = '+628561435232';
+		$FROM = '+12027596195';
+		$output = shell_exec("curl -X POST '".$this->API_URL."' --data-urlencode 'To=".$TO."' 
+							--data-urlencode 'From=".$FROM."' --data-urlencode 
+							'Body=".$body."' -u ".$this->API_AUTH);
+		return $output;
+	}
+
+	public function recv() {
+		$hasil = [];
+
+		$output = shell_exec("curl -X GET '".$this->API_URL."' -u ".$this->API_AUTH);
+		$hasil_json = json_decode($output);
+
+		foreach ($hasil_json->messages as $row) {
+			$temp = [];
+			if ($row->status == "received") {
+				$temp['sid'] = $row->sid;
+				$temp['date_created'] = $row->date_created;
+				$temp['date_updated'] = $row->date_updated;
+				$temp['date_sent'] = $row->date_sent;
+				$temp['body'] = $row->body;
+			}
+			$hasil[] = $temp;
+		}
+		return $hasil;
+	}
+}
